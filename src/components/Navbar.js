24c17ceed1632e90d8cb768f4b6778a976cd5592
praveenmundkar <pravinbiloli123@gmail.com>
2021-11-14 17:56:40 +0530

    deploy touch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,10 +17,10 @@ export default function Navbar(props) {
                             <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">About</a>
+                            <a className="nav-link" href="#">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">Contact</a>
+                            <a className="nav-link" href="#">Contact</a>
                         </li>
                         
                     </ul>
